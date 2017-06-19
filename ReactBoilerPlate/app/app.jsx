3aa4cb4f03493823bc7eb2ab391ceca78d3015af
Eduardo Junque <eduardo.junque@navegarti.com.br>
2017-06-19 10:52:51 -0300

    Incluindo stage-0 babel pack

--- ReactBoilerPlate/app/app.jsx
+++ ReactBoilerPlate/app/app.jsx
@@ -1,6 +1,18 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var objOne = {
+  name: 'Dudy',
+  location: 'Campinas'
+};
+
+var objTwo = {
+  age: 29,
+  ...objOne
+};
+
+console.log(objTwo);
+
 ReactDOM.render(
   <h1>React BoilerPlate</h1>,
   document.getElementById('app')
