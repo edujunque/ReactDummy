7e177eee8929e53a3024f3152cbbea1079537ca9
Eduardo Junque <eduardo.junque@navegarti.com.br>
2017-06-20 13:41:26 -0300

    Refactoring do app usando arrow function e stateless functions

--- ReactWeather/app/components/About.jsx
+++ ReactWeather/app/components/About.jsx
@@ -1,11 +1,15 @@
 var React = require('react');
 
-var About = React.createClass({
-  render: function (){
-    return(
-      <h3>About Component</h3>
-    );
-  }
-});
+// var About = React.createClass({
+//   render: function (){
+//
+//   }
+// });
+//Só da pra usar dessa forma abaixo pois o componente não gerencia nenhum state, caso contrario tem que usar o exemplo acima
+var About = (props) => {
+  return(
+    <h3>About Component</h3>
+  );
+};
 
 module.exports = About;
--- ReactWeather/app/components/WeatherMessage.jsx
+++ ReactWeather/app/components/WeatherMessage.jsx
@@ -1,15 +1,36 @@
 var React = require('react');
 
-var CityMessage = React.createClass({
-  render: function(){
-    var {location,temp} = this.props;
+// var CityMessage = React.createClass({
+//   render: function(){
+//     var {location,temp} = this.props;
+//
+//     return(
+//       <div>
+//         <p>it's it {temp} in {location}</p>
+//       </div>
+//     );
+//   }
+// });
 
-    return(
-      <div>
-        <p>it's it {temp} in {location}</p>
-      </div>
-    );
-  }
-});
+// var WeatherMessage = (props) => {
+//   var {location,temp} = props;
+//   return(
+//     <div>
+//       <p>it's it {temp} in {location}</p>
+//     </div>
+//   );
+// };
 
-module.exports = CityMessage;
+//da pra colocar {location,temp} na chamada da função pois já existe
+//uma funcionalidade da propria linguagem que pega automaticamente
+//props na chamada da função, então conseguimos usar o disruption
+//para pegar os valores que queremos através do nome da propriedade
+var WeatherMessage = ({location,temp}) => {
+  return(
+    <div>
+      <p>it's it {temp} in {location}</p>
+    </div>
+  );
+};
+
+module.exports = WeatherMessage;
